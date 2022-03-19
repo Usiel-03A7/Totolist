ea10e0840fea479e854b998ba359ebffc49637e2
Sebastian Solano <shebastyan@gmail.com>
2022-03-19 15:24:33 -0600

    try tha new

--- js/app.js
+++ js/app.js
@@ -1,32 +1,56 @@
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
     const form = document.querySelector('#task-form')
     const input = document.querySelector('#task-input')
     const listEle = document.querySelector('#tasks')
-    form.addEventListener('submit',(e)=> {
+    form.addEventListener('submit', (e) => {
         e.preventDefault();
 
         const task = input.value;
 
-        if(!task){
+        if (!task) {
             alert('Se recomienda completar campos');
             return;
         }
 
         const taskElem = document.createElement('DIV');
-        taskElem.classList.add(task);
+        taskElem.classList.add('task');
 
         const taskConstEle = document.createElement('DIV');
         taskConstEle.classList.add('contenedor');
-        taskConstEle.innerText=task;
+
 
         taskElem.appendChild(taskConstEle);
-        const imputEl = document.createElement('input');
-        imputEl.classList.add('text')
-        imputEl.type= 'text';
-        imputEl.value=task;
-        imputEl.setAttribute('readonly','readonly');
-         
+
+        const tImputEl = document.createElement('input');
+        tImputEl.classList.add('text')
+        tImputEl.type = 'text';
+        tImputEl.value = task;
+        tImputEl.setAttribute('readonly', 'readonly')
+
+
+
+        taskConstEle.appendChild(tImputEl);
+
+        /*        */
+        const acccion = document.createElement('div')
+        acccion.classList.add('actions')
+
+        const edit = document.createElement('button')
+        edit.classList.add('edit')
+        edit.innerHTML = 'Editar'
+
+        const delet = document.createElement('button')
+        delet.classList.add('delete');
+        delet.innerHTML = 'Eliminar';
+
+        acccion.appendChild(edit);
+        acccion.appendChild(delet);
+        taskElem.appendChild(acccion);
+        listEle.appendChild(taskElem)
         listEle.appendChild(taskElem);
+        input.value = '';
+
+
     })
 
 })
